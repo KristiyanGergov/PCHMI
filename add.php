863c3d750bf724bd6955d61dae4a606f182bd2a9
Sophie Peneva <sophie@lab08.com>
2020-05-28 21:21:11 +0300

    add fe part

--- add.php
+++ add.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title>Title</title>
     <link rel="stylesheet" type="text/css" href="styles/index.css">
     <link rel="stylesheet" type="text/css" href="styles/body.css">
@@ -18,8 +18,8 @@
 }
 </style>";
 ?>
-<?php include "header.html"; ?>
-<?php include "topnavigation.html"; ?>
+<?php include "view/header.html"; ?>
+<?php include "view/topnavigation.html"; ?>
 
 <br/>
 
@@ -28,16 +28,33 @@
         <img src="pictures/aeon.png" alt="aeon">
     </div>
 
-    <div class="column" style="text-align: center; background-color:#bbb;">
-        <label for="productName"><b>Name of product</b></label>
-        <input id="productName" type="text" placeholder="Enter Product name" name="productName" required>
+    <form action="actions/add_item.php" method="POST" enctype="multipart/form-data">
+        <div class="column" style="text-align: center; background-color:#bbb;">
+            <label for="productName"><b>Name of product</b></label>
+            <input id="productName" type="text" placeholder="Enter Product name" name="name" required>
 
-        <label for="psw"><b>Price for renting</b></label>
-        <input id="psw" type="text" placeholder="Enter Price for renting" name="psw" required>
+            <label for="description"><b>Description</b></label>
+            <input id="description" type="text" placeholder="Enter product description" name="description" required>
 
-        <label for="available" class="available">Available for rent</label>
-        <input id="available" type="checkbox">
-    </div>
+            <label for="price"><b>Price for renting</b></label>
+            <input id="price" type="text" placeholder="Enter Price for renting" name="price" required>
+
+            <label for="available" class="available">Available for rent</label>
+            <input id="available" type="checkbox" name="available">
+
+            <label for="type">Choose a type:</label>
+
+                <select name="type" id="type">
+                <option value="shisha">Shisha</option>
+                <option value="bowl">Bowl</option>
+                <option value="service">Service</option>
+                <option value="consumable">Consumable</option>
+                </select>
+
+            <input type="file"  class="field" name="image" required>
+                <button type="submit">Add</button>
+        </div>
+    </form>
 </div>
 
 </body>
