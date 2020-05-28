ecfedf3767f2b16c4b753058ebe62a8cbf569524
kristiyan.gergov <kristiyan.gergov98@gmail.com>
2020-05-28 23:04:05 +0300

    Styled add.php

--- add.php
+++ add.php
@@ -1,9 +1,9 @@
 <?php
-    session_start();
+session_start();
 
-if(!isset($_SESSION["token"])) {
-    header("Location: index.php");
-}
+//if (!isset($_SESSION["token"])) {
+//    header("Location: add.php");
+//}
 ?>
 
 <!DOCTYPE html>
@@ -15,6 +15,26 @@ if(!isset($_SESSION["token"])) {
     <link rel="stylesheet" type="text/css" href="styles/body.css">
     <link rel="stylesheet" type="text/css" href="styles/navigation.css">
     <link rel="stylesheet" type="text/css" href="styles/input.css">
+    <link rel="stylesheet" type="text/css" href="styles/checkbox.css">
+    <link rel="stylesheet" type="text/css" href="styles/button.css">
+    <script class="jsbin" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script class="jsbin" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.0/jquery-ui.min.js"></script>
+    <script>
+        function readURL(input) {
+            if (input.files && input.files[0]) {
+                const reader = new FileReader();
+
+                reader.onload = function (e) {
+                    $('#selectedImage')
+                        .attr('src', e.target.result)
+                        .width(150)
+                        .height(200);
+                };
+
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+    </script>
 </head>
 <body>
 
@@ -31,36 +51,42 @@ if(!isset($_SESSION["token"])) {
 
 <br/>
 
-<div class="row">
-    <div class="column" style="background-color:#aaa;">
-        <img src="pictures/aeon.png" alt="aeon">
+<div class="row" style="background-color:#aaa;">
+    <div class="column">
+        <img style="display:block;margin:auto; border: 3px solid #ddd;" id="selectedImage" src="pictures/plus.png" alt="plus">
     </div>
 
     <form action="actions/add_item.php" method="POST" enctype="multipart/form-data">
-        <div class="column" style="text-align: center; background-color:#bbb;">
-            <label for="productName"><b>Name of product</b></label>
-            <input id="productName" type="text" placeholder="Enter Product name" name="name" required>
+        <div>
+            <div class="column">
+                <label for="productName"><b>Name of product</b></label>
+                <input id="productName" type="text" placeholder="Enter Product name" name="name" required>
 
-            <label for="description"><b>Description</b></label>
-            <input id="description" type="text" placeholder="Enter product description" name="description" required>
+                <label for="description"><b>Description</b></label>
+                <input id="description" type="text" placeholder="Enter product description" name="description" required>
 
-            <label for="price"><b>Price for renting</b></label>
-            <input id="price" type="text" placeholder="Enter Price for renting" name="price" required>
+                <label for="price"><b>Price for renting</b></label>
+                <input id="price" type="text" placeholder="Enter Price for renting" name="price" required>
 
-            <label for="available" class="available">Available for rent</label>
-            <input id="available" type="checkbox" name="available">
+                <label style="margin-top: 10px" class="checkboxContainer">Available for rent
+                    <input type="checkbox" checked="checked">
+                    <span class="checkmark"></span>
+                </label>
 
-            <label for="type">Choose a type:</label>
+                <label style="margin-top: 10px;">
+                    <span style="font-size: 22px;">Choose a type:</span>
+                    <select style="font-size: 15px; margin-left: 10px" class="custom-select" name="type" id="type">
+                        <option value="shisha">Shisha</option>
+                        <option value="bowl">Bowl</option>
+                        <option value="service">Service</option>
+                        <option value="consumable">Consumable</option>
+                    </select>
+                </label>
 
-                <select name="type" id="type">
-                <option value="shisha">Shisha</option>
-                <option value="bowl">Bowl</option>
-                <option value="service">Service</option>
-                <option value="consumable">Consumable</option>
-                </select>
-
-            <input type="file"  class="field" name="image" required>
-                <button type="submit">Add</button>
+                <input onchange="readURL(this);" style="margin-top: 20px" type="file" class="field" name="image"
+                       required>
+                <button style="margin-top: 20px" class="button" type="submit">Add</button>
+            </div>
         </div>
     </form>
 </div>
