2674b75a90161074d00f2e37b5fd368ebd7eb080
Emil Zhivkov <emil.zhivkov@ocado.com>
2020-05-28 18:48:05 +0300

    moved the aws ssh token inside of the deploy dir

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,4 @@
 
-chmod 400 aws_fmi_project.pem
+chmod 400 deploy/aws_fmi_project.pem
 
-ssh -i aws_fmi_project.pem ubuntu@ec2-3-22-254-117.us-east-2.compute.amazonaws.com 'cd /opt/lampp/htdocs/project && sudo git pull origin master'
+ssh -i deploy/aws_fmi_project.pem ubuntu@ec2-3-22-254-117.us-east-2.compute.amazonaws.com 'cd /opt/lampp/htdocs/project && sudo git pull origin master'
