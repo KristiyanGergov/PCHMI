6b40e69364302df8d65b95b25a6d8ff7a8e177d4
Viktor Mirev <mirev@uber.com>
2020-05-29 23:02:29 +0300

    picture sizing

--- actions/generate_item_list.php
+++ actions/generate_item_list.php
@@ -6,7 +6,7 @@ function generate_item_div($item)
 {
     return
     '<div class="column" style="background-color:#aaa;">
-        <a href="details.php?id='. $item->id .'"><img src="'. $item->image .'" alt="aeon"></a>
+        <a href="details.php?id='. $item->id .'"><img src="'. $item->image .'"max-width="300px" max-height="250px" overflow:hidden alt="aeon"></a>
         <div class="name">'. $item->name .'</div>
         <div class="price">'. $item->price .'</div>
     </div>'."\n   ";
